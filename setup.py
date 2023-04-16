b2607ed702dd1b7710189c08e9bdef071d42e2a2
Forest Rouse <forest@layerjot.com>
2023-04-16 11:11:53 -0700

    Change around requirements to use latest versions

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     ext_modules=[],
     cmdclass={'build_ext': BuildExtension.with_options(no_python_abi_suffix=True)},
     install_requires=[
-        'torch==1.11.0',
-        'torchvision==0.12.0',
-        'natsort==7.1.0',
-        'pyarrow==10.0.0',
-        'scikit-learn==1.1.2',
-        'efficientnet_pytorch==0.7.1',
+        #'torch==1.11.0',
+        #'torchvision==0.12.0',
+        #'natsort==7.1.0',
+        #'pyarrow==10.0.0',
+        #'scikit-learn==1.1.2',
+        #'efficientnet_pytorch==0.7.1',
         # 'efficientnet-lite-pytorch==0.1.0',
         # 'efficientnet-lite0-pytorch-model==0.1.0',
         # 'efficientnet-lite2-pytorch-model==0.1.0'
